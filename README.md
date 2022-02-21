f002d9ea8a86b4df7b80c3eccb731e33fc65272f
Arthur Mello <melloarthur@riseup.net>
2022-02-20 21:54:59 -0300

    Readme e correção no Tradutor

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ A foto do robô deve ser colocada no lugar de 'robo.png' e a foto da mesa de des
 
 Para rodar o mapa, basta executar o 'app.py' e tracejar sua rota na tela. O programa irá gerar um arquivo de texto com as instruções inseridas. Depois, o 'tradutor.py' poderá ser executado para converter estas instruções em um código Python.
 
+Antes de usar, no 'app.py' defina o tamanho da mesa que você está usando, assim como o tamanho do seu robô, ambos em cm.
+
 Caso seja necessário mudar as imagens, recomendo que utilize as extensões .png ou .jpg para evitar bugs. Este ainda é um projeto em desenvolvimento.
--- tradutor.py
+++ tradutor.py
@@ -27,28 +27,29 @@ baseStr = base.read()
 
 final.write(f'{str(baseStr)}\n')
 
-Splitado = []
-Splitado.append(splitadoCru[0])
-Splitado.append(splitadoCru[2])
-Splitado.append(splitadoCru[1])
-
-del splitadoCru[0]
-del splitadoCru[0]
-del splitadoCru[0]
+Retas = []
+Curvas = []
 
 for i in range(len(splitadoCru)):
-    Splitado.append(splitadoCru[i])
+    movimento = splitadoCru[i].split(' ')
+    if movimento[0].startswith('R'):
+        Retas.append(movimento[-1])
+    else:
+        Curvas.append(splitadoCru[i])
 
+Curvas.append('E 0')
 
-for i in range(len(Splitado)):
-    acao = Splitado[i]
-    valor = acao.split(' ')[-1]
+for i in range(len(Retas)):
+    reta = Retas[i]
+    curva = Curvas[i]
+    final.write(f'\nAcelera(600, {reta}) # Robô segue em frente por {reta} mm.')
 
-    if acao.startswith('R'):
-        final.write(f'\nAcelera(600, {valor}) # Robô segue em frente por {valor} mm.')
-    if acao.startswith('E'):
+    if curva.startswith('E'):
+        valor = curva.split(' ')[-1]
         final.write(f"\nCurva(600, {valor}, 'left') # Robô realiza curva de {valor}° para a esquerda.")
-    if acao.startswith('D'):
+
+    if curva.startswith('D'):
+        valor = curva.split(' ')[-1]
         final.write(f"\nCurva(600, {valor}, 'right') # Robô realiza curva de {valor}° para a direita.")
 
 final.write('\n\n# FIM DA PROGRAMAÇÃO | GERADO AUTMATICAMENTE POR LYCAON #')
